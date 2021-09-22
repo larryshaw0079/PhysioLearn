3f724c26fa36ff9d42b2a05b039bb220011a4211
larryshaw0079 <larryshaw0079@live.com>
2021-09-22 11:37:15 +0800

    feat: add learning_rate_adjust

--- physiossl/training/utils.py
+++ physiossl/training/utils.py
@@ -4,14 +4,16 @@
 @Software: PyCharm
 @Desc    : 
 """
+import math
 import warnings
 import random
+from typing import List
 
 import torch
 import numpy as np
 
 
-def setup_seed(seed):
+def setup_seed(seed: int):
     warnings.warn(f'You have chosen to seed ({seed}) training. This will turn on the CUDNN deterministic setting, '
                   f'which can slow down your training considerably! You may see unexpected behavior when restarting '
                   f'from checkpoints.')
@@ -21,3 +23,20 @@ def setup_seed(seed):
     np.random.seed(seed)
     random.seed(seed)
     torch.backends.cudnn.deterministic = True
+
+
+def adjust_learning_rate(optimizer: torch.optim.Optimizer, lr: float, epoch: int, total_epochs: int, lr_schedule: List,
+                         method: str = 'lambda'):
+    """Decay the learning rate based on schedule"""
+    assert method in ['lambda', 'cos']
+
+    if method == 'cos':  # cosine lr schedule
+        lr *= 0.5 * (1. + math.cos(math.pi * epoch / total_epochs))
+    elif method == 'lambda':  # stepwise lr schedule
+        for milestone in lr_schedule:
+            lr *= 0.1 if epoch >= milestone else 1.
+    else:
+        raise ValueError
+
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
